055a1208135fd0a1352891d6783002691d5cdfd2
germany900 <ninganegro@hotmail.com>
2019-06-13 12:37:00 -0500

    todo listo para integrar login con terceros

--- common/models/ykeky_articulos.js
+++ common/models/ykeky_articulos.js
@@ -3,4 +3,31 @@
 module.exports = function(Ykekyarticulos) {
 
     Ykekyarticulos.disableRemoteMethodByName('deleteById');
+
+
+    Ykekyarticulos.sendEmail = function(msg, cb) {
+        Ykekyarticulos.app.models.Email.send({
+            to: "carmelo402.ja@gmail.com, lonch97@gmail.com", 
+            from: "termineitor009@gmail.com",
+            subject: "Your custom email subject here",
+            text: "Todo el mundo sabe que el negro es muuuuuuuuuuuuuuuuy gay xD",
+            html: msg
+        }, function(err, mail) {
+            console.log('email sended !!!');
+            if(err)return err;
+        });
+        cb(null,'mesaje que muestra la apo, solo para probar....' + msg);
+    }
+
+    Ykekyarticulos.remoteMethod(
+        'sendEmail',
+        {
+            http: {path:'/sendEmail',verb:'get'},
+            description:[
+                "api para envio de email."
+            ],
+            accepts: {arg: 'msg', type:'string'},
+            returns: {arg: 'Email', type: 'string'}
+        }
+    );
 };
--- common/models/ykeky_user.js
+++ common/models/ykeky_user.js
@@ -1,7 +1,39 @@
 'use strict';
 
+const config = require('../../server/config.json');
+const path = require('path');
+
 module.exports = function(Ykekyuser) {
 
+  
 
-    
+    //send verification email after registration
+    Ykekyuser.afterRemote('create', function(context, userInstance, next) {
+      console.log('> Ykekyuser.afterRemote triggered');
+  
+      var verifyOptions = {
+        type: 'email',
+        to: userInstance.email,
+        from: 'termineitor009@gmail.com',
+        subject: 'Thanks for registering.',
+        template: path.resolve(__dirname, '../../server/views/verify.ejs'),
+        redirect: '/verified',
+        user: userInstance
+      };
+  
+      userInstance.verify(verifyOptions, function(err, response, next) {
+        if (err) return next(err);
+  
+        console.log('> verification email sent:', response);
+  
+        context.res.render('response', {
+          title: 'Signed up successfully',
+          content: 'Please check your email and click on the verification link ' -
+              'before logging in.',
+          redirectTo: '/',
+          redirectToLinkText: 'Log in'
+        });
+      });
+    });
+ 
 };
