c2a4f21b93274a540292d35c5888cdc54b9388c5
germany900 <ninganegro@hotmail.com>
2019-06-13 13:58:17 -0500

    problemas con session para el login con terceros

--- server/server.js
+++ server/server.js
@@ -2,8 +2,42 @@
 
 var loopback = require('loopback');
 var boot = require('loopback-boot');
-
+var path = require('path');
 var app = module.exports = loopback();
+
+var PassportConfigurator = require('loopback-component-passport').PassportConfigurator;
+var passportConfigurator = new PassportConfigurator(app);
+
+boot(app, __dirname);
+
+// Enable http session
+app.use(loopback.session({ secret: 'CFD]0czamsyMu&[4a;bJqe0~&DV4=w' }));
+
+// Load the provider configurations
+var config = {};
+try {
+ config = require('./providers.json');
+} catch(err) {
+ console.error('Please configure your passport strategy in `providers.json`.');
+ console.error('Copy `providers.json.template` to `providers.json` and replace the clientID/clientSecret values with your own.');
+ process.exit(1);
+}
+// Initialize passport
+passportConfigurator.init();
+
+// Set up related models
+passportConfigurator.setupModels({
+ userModel: app.models.ykeky_user,
+ userIdentityModel: app.models.userIdentity,
+ userCredentialModel: app.models.UserCredential
+});
+// Configure passport strategies for third party auth providers
+for(var s in config) {
+ var c = config[s];
+ c.session = c.session !== false;
+ passportConfigurator.configureProvider(s, c);
+}
+
 
 app.start = function() {
   // start the web server
